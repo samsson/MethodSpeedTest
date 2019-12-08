49af17a978edb0b718bf83175f78066e72a3f163
Sami <sami.ruohonen16@gmail.com>
2019-12-08 15:08:32 +0200

    Changin average to median

--- src/speedtest/LibraryFunctions.java
+++ src/speedtest/LibraryFunctions.java
@@ -5,4 +5,50 @@ public class LibraryFunctions {
     public static void main(String[] args) {
         
     }
+
+    public static String parseCommandArguments(final String parse_command_line)
+    {
+        try
+        {
+            //if (parse_command_line.startsWith("\""))
+            if (parse_command_line.matches("^\""))
+            {
+                // Take everything after quotes and a trailing space
+                final String[] command_line_parsed = parse_command_line.split("^\".*?\" ", 2);
+                return command_line_parsed[1];
+            }
+            else
+            {
+                // No starting quote found so take everything after first space
+                final String[] command_line_parsed = parse_command_line.split(" ", 2);
+                return command_line_parsed[1];
+            }
+        }
+        catch(final Exception ex)
+        {
+            // Exception, most likely index due missing arguments, return empty arguments
+            return "";
+        }
+    }
+    
+    public static boolean equalsfunction(final String commandline, final String commandLineArguments)
+    {
+        final String commandLineNew = parseCommandArguments(commandline);
+        if (commandLineNew.equals(commandLineArguments))
+        {
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean matchesfunction(final String commandline)
+    {
+        // .*calc\\.exe
+        if (commandline.matches(".*calc\\.exe"))
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/speedtest/SpeedTest.java
+++ src/speedtest/SpeedTest.java
@@ -1,5 +1,8 @@
 package speedtest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpeedTest {
     static String commandLine = "\"C:\\Windows\\system32\\cmd.exe\" /c calc.exe";
     static String commandLineArguments = "/c calc.exe";
@@ -10,64 +13,36 @@ public class SpeedTest {
         return (int)value / count;
 
     }
+    public static float getPercentage(int val1, int val2, int size)
+    {
+        final double q = (((double)averageSum(val1, size) - averageSum(val2, size)) / averageSum(val1, size))*100;
+        return (float)q;
+    }
 
-    // Parse commandline arguments from CommandLine fields
-    public static String parseCommandArguments(final String parse_command_line)
-    {
-        try
-        {
-            //if (parse_command_line.startsWith("\""))
-            if (parse_command_line.matches("^\""))
-            {
-                // Take everything after quotes and a trailing space
-                final String[] command_line_parsed = parse_command_line.split("^\".*?\" ", 2);
-                return command_line_parsed[1];
-            }
-            else
-            {
-                // No starting quote found so take everything after first space
-                final String[] command_line_parsed = parse_command_line.split(" ", 2);
-                return command_line_parsed[1];
-            }
+    public static double median(List<Long> a){
+        int middle = a.size()/2;
+ 
+        if (a.size() % 2 == 1) {
+            return a.get(middle);
+        } else {
+           return (a.get(middle-1) + a.get(middle)) / 2.0;
         }
-        catch(final Exception ex)
-        {
-            // Exception, most likely index due missing arguments, return empty arguments
-            return "";
-        }
-    }
-    public static boolean equalsfunction(final String commandline, final String commandLineArguments)
-    {
-        final String commandLineNew = parseCommandArguments(commandline);
-        if (commandLineNew.equals(commandLineArguments))
-        {
-                return true;
-        }
-        return false;
-    }
-    public static boolean matchesfunction(final String commandline)
-    {
-        // .*calc\\.exe
-        if (commandline.matches(".*calc\\.exe"))
-        {
-            return true;
-        }
-        return false;
     }
 
     public static void main(final String[] args) {
+
         int a = 0;
         boolean equalsFunction = false;
         boolean matchesFunction = false;
-        long equalstime = 0;
-        long matchestime = 0;
+        List<Long> run1 = new ArrayList<Long>();
+        List<Long> run2 = new ArrayList<Long>();
         long startTime;
-        long equalSum = 0;
-        long matchesSum = 0;
+        long runtime1 = 0;
+        long runtime2 = 0;
 
         System.out.println("Preparing tests ");
-        equalsFunction = equalsfunction(commandLine, commandLineArguments);
-        matchesFunction = matchesfunction(commandLine);
+        equalsFunction = LibraryFunctions.equalsfunction(commandLine, commandLineArguments);
+        matchesFunction = LibraryFunctions.matchesfunction(commandLine);
         System.out.println("Ok Running test: ");
         
         do {
@@ -75,47 +50,58 @@ public class SpeedTest {
             {
                 startTime = System.nanoTime();
                 /* Your function call here */
-                equalsFunction = equalsfunction(commandLine, commandLineArguments);
-                equalstime = equalstime + System.nanoTime() - startTime;
+                equalsFunction = LibraryFunctions.equalsfunction(commandLine, commandLineArguments);
+                
+                
+                runtime1 = runtime1 +  System.nanoTime() - startTime;
             }
 
             for(int i = 0; i < 1000; ++i)
             {
                 startTime = System.nanoTime();
                 /* Your second function call here */
-                matchesFunction = matchesfunction(commandLine);
-                matchestime = matchestime + System.nanoTime() - startTime;
+                matchesFunction = LibraryFunctions.matchesfunction(commandLine);
+                
+                
+                runtime2 = runtime2 + System.nanoTime() - startTime;
             }
 
-            System.out.println("Equals & matches in milli: " + equalsFunction + " " + equalstime + " " + matchesFunction + " " + matchestime + " "); 
-            // Calculate average result
-            equalSum = equalSum + equalstime;
-            matchesSum = matchesSum + matchestime;
+            System.out.println("Equals & matches in milli: " + equalsFunction + " " + runtime1 + " " + matchesFunction + " " + runtime2 + " "); 
 
-            equalstime = 0;
-            matchestime = 0;
+            run1.add(runtime1);
+            run2.add(runtime2);
+            runtime1 = 0;
+            runtime2 = 0;
+            /* 
             try {
 				Thread.sleep(100);
 			} catch (final InterruptedException e) {
 				e.printStackTrace();
             }
+             */
             a++;
           }
-        while (a < 100);
+        while (a < 500);
+        
+        int runMedian1 = (int)median(run1);
+        int runMedian2 = (int)median(run2);
+        
         System.out.println("Test complete.");
         //System.out.println("Lowest numbers in millisec out of: " + a + " rounds, Equals: " + lowestEqual + " Matches: " + lowestMatches);
-        System.out.println("Average result Equals: " + averageSum(equalSum, a)/100000 + "ms Matches: " + averageSum(matchesSum, a)/100000+ "ms");
-        if (averageSum(equalSum, a) < averageSum(matchesSum, a))
+        System.out.println("Median result Equals: " + median(run1)/100000 + "ms Matches: " + median(run2)/100000+ "ms");
+        
+        if (runMedian1 < runMedian2)
         {
+            System.out.println("Equals is " + getPercentage(runMedian2, runMedian1, a) + " percent faster.");
+            /* 
             final double q = (((double)averageSum(matchesSum, a) - averageSum(equalSum, a)) / averageSum(matchesSum, a))*100;
             final float percentage2 = (float)q;
             System.out.println("Equals is " + percentage2 + " percent faster.");
+             */
         } 
         else
         {
-            final double q = (((double)averageSum(equalSum, a) - averageSum(matchesSum, a)) / averageSum(equalSum, a))*100;
-            final float percentage2 = (float)q;
-            System.out.println("Matches is " + percentage2 + " percent faster.");
+            System.out.println("Matches is " + getPercentage(runMedian1, runMedian2, a) + " percent faster.");
         }
 
     }
